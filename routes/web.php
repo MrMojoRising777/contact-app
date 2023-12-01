a166e8a371b0572506dba1f83906a52f19d7f5da
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-01 15:33:07 +0100

    alternative passing data to view

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+function getContacts() {
+    return [
+        1 => ['name' => 'Name 1', 'phone' => '123456789'],
+        2 => ['name' => 'Name 2', 'phone' => '987654321'],
+        3 => ['name' => 'Name 3', 'phone' => '147258369'],
+    ];
+}
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/contacts', function () {
-    $contacts = [
-        1 => ['name' => 'Name 1', 'phone' => '123456789'],
-        2 => ['name' => 'Name 2', 'phone' => '987654321'],
-        3 => ['name' => 'Name 3', 'phone' => '147258369'],
-    ];
+    $contacts = getContacts();
     return view('contacts.index', compact('contacts')); // alternative to compact(): ['contacts' => $contacts]
 })->name('contacts.index');
 
@@ -31,5 +35,8 @@ Route::get('/contacts/create', function () {
 })->name('contacts.create');
 
 Route::get('/contacts/{id}', function ($id) {
-    return "Contact " . $id;
+    $contacts = getContacts();
+    abort_if(!isset($contacts[$id]), 404); // if id does not exist in contacts[], give 404 error | alternative: abort_unless($contacts[$id], 404)
+    $contact = $contacts[$id];
+    return view('contacts.show')->with('contact', $contact);
 })->name('contacts.show');
