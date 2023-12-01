c44a65d8d141ca97a161f500696165b56f88be03
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-01 14:57:19 +0100

    creating first view

--- routes/web.php
+++ routes/web.php
@@ -14,32 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $html = "
-        <h1>Contact App</h1>
-        <div>
-            <a href='" . route('contacts.index') . "'>All contacts</a>
-            <a href='" . route('contacts.create') . "'>Add contact</a>
-            <a href='" . route('contacts.show', 1) . "'>Show contact</a>
-        </div>
-    ";
-    return $html;
+    return view('welcome');
 });
 
-Route::prefix('admin')->group( function () {
-    Route::get('/contacts', function () {
-        return "<h1>All contacts</h1>";
-    })->name('contacts.index');
-    
-    Route::get('/contacts/create', function () {
-        return "<h1>Add new contact</h1>";
-    })->name('contacts.create');
-    
-    Route::get('/contacts/{id}', function ($id) {
-        return "Contact " . $id;
-    })->name('contacts.show');
-});
+Route::get('/contacts', function () {
+    return view('contacts.index'); // contacts is the folder, index is the file name
+})->name('contacts.index');
 
-// if route does not exist, show this route
-Route::fallback(function() {
-    return "<h1>Sorry, the page does not exist</h1>";
-});
+Route::get('/contacts/create', function () {
+    return "<h1>Add new contact</h1>";
+})->name('contacts.create');
+
+Route::get('/contacts/{id}', function ($id) {
+    return "Contact " . $id;
+})->name('contacts.show');
