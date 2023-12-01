555465c65672a8e65dba5ca47e3a20ad4124f442
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-01 15:54:45 +0100

    blade template inheritance

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -47,7 +47,7 @@
       </div>
     </nav>
 
-    {{-- content --}}
+    @yield('content') {{-- content --}}
 
     <script src="{{ asset('js/jquery.min.js') }}"></script>
     <script src="{{ asset('js/popper.min.js') }}"></script>
