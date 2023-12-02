f2780223b9762718eed7186f9f9d0de4b0c09e2f
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-02 15:56:26 +0100

    resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +25,8 @@ Route::controller(ContactController::class)->name('contacts.')->group(function (
     Route::get('/contacts/create', 'create')->name('create');
     Route::get('/contacts/{id}', 'show')->name('show');
 });
+Route::resource('/companies', CompanyController::class);
+Route::resource([
+    '/tags' => TagController::class,
+    '/tasks' => TaskController::class
+]);
