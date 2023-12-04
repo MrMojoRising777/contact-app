d70f71a454905808fc260ab284b4771fa6ff1e6b
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-04 19:49:15 +0100

    Exercise 7 - Calling model in controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Repositories\CompanyRepository;
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
@@ -21,7 +22,7 @@ class ContactController extends Controller
         //     2 => ['name' => 'Company Two', 'contacts' => 5],
         // ];
         $companies = $this->company->pluck();
-        $contacts = $this->getContacts();
+        $contacts = Contact::latest()->get();
         return view('contacts.index', compact('contacts', 'companies'));
     }
 
@@ -32,17 +33,7 @@ class ContactController extends Controller
 
     public function show($id) 
     {
-        $contacts = $this->getContacts();
-        abort_if(!isset($contacts[$id]), 404);
-        $contact = $contacts[$id];
+        $contact = Contact::findOrFail($id);
         return view('contacts.show')->with('contact', $contact);
-    }
-
-    protected function getContacts() {
-        return [
-            1 => ['id' => 1, 'name' => 'Name 1', 'phone' => '123456789'],
-            2 => ['id' => 2, 'name' => 'Name 2', 'phone' => '987654321'],
-            3 => ['id' => 3, 'name' => 'Name 3', 'phone' => '147258369'],
-        ];
     }
 }
--- app/Repositories/CompanyRepository.php
+++ app/Repositories/CompanyRepository.php
@@ -1,13 +1,12 @@
 <?php
 namespace App\Repositories;
 
+use App\Models\Company;
+
 class CompanyRepository
 {
   public function pluck()
   {
-    return [
-      1 => 'Company One',
-      2 => 'Company Two',
-    ];
+    return Company::orderBy('name')->pluck('name', 'id');
   }
 }
