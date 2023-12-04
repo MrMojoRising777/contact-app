66e297dd30f88b6449ebcca57a9f318a0e4dd125
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-04 14:09:43 +0100

    Mass assignment

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -11,4 +11,10 @@ class Company extends Model
 
     // protected $table = "app_companies";
     // protected $primaryKey = "_id";
+    // protected $guarded = [];  // this only allows existing columns to be changed in mass assignment
+    protected $fillable = ['name', 'email', 'address', 'website'];  // specify what columns can be changed in mass assignment
+
+    // when mass assigning, if a non-existing column is updated
+    //  $fillable will run and update the existing columns
+    //  $guarded throws an exception
 }
