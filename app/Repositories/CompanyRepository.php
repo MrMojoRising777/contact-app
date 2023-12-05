866ea0d743e44b887ee6267da4748eaf4159f2c1
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-05 12:48:03 +0100

    Eloquent relationship - implementing Eloquent relationship

--- app/Repositories/CompanyRepository.php
+++ app/Repositories/CompanyRepository.php
@@ -7,6 +7,14 @@ class CompanyRepository
 {
   public function pluck()
   {
-    return Company::orderBy('name')->pluck('name', 'id');
+    // return Company::orderBy('name')->pluck('name', 'id');
+
+    // display number of contacts in dropdown menu
+    $data = [];
+    $companies = Company::orderBy('name')->get();
+    foreach ($companies as $company) {
+      $data[$company->id] = $company->name . " (" . $company->contacts()->count() . ")" ;
+    }
+    return $data;
   }
 }
