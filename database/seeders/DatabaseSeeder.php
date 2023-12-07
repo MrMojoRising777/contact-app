4f050e979c72fad6eccb022dbe12ed864453c20e
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-07 11:41:05 +0100

    Implementing eloquent factories in Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Company;
+use App\Models\Contact;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,10 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            CompanySeeder::class,
-            ContactSeeder::class,
-        ]);
-
+        Company::factory()->count(10)->create();
+        Contact::factory()->count(100)->create();
     }
 }
