23e69ba717eceaa4da484ab97dc13dfb6842d1f6
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-12 23:07:56 +0100

    Adding User Registration feature

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\TagController;
