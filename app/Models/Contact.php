8d754cdad4fae700c62a669122abe1ae46d9b7c8
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-12 11:36:01 +0100

    Local Scopes

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Scopes\SimpleSoftDeletingScope;
+use Illuminate\Database\Eloquent\Builder;
 
 class Contact extends Model
 {
@@ -22,5 +23,18 @@ class Contact extends Model
     public function tasks()
     {
         return $this->hasMany(Tasks::class);
+    }
+
+    public function scopeSortByNameAlpha(Builder $query)
+    {
+        return $query->orderBy('first_name');
+    }
+
+    public function scopeFilterByCompany(Builder $query)
+    {
+        if ($companyId = request()->query("company_id")) {
+            $query->where("company_id", $companyId);
+        }
+        return $query;
     }
 }
