ac2f50325bae01797f8769fdd5e6354eb61be435
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-12 20:41:31 +0100

    Setting up Fortify

---
+++ chapter_Authentication.php
@@ -0,0 +1,9 @@
+Alternatives;
+
+Laravel Breeze
+Laravel Jetstream
+Laravel UI
+
+using Laravel Fortify
+> composer require laravel/fortify
+> php artisan vendor:publish --provider="Laravel\Fortify\FortifyServiceProvider"
