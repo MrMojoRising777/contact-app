ec156e6dd79f3da4c00526a363d4e5375e191207
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-15 14:08:09 +0100

    Debugging queries (laravel-debugbar)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 
@@ -21,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFour();
+
+        // DEBUGGING QUERIES
+        // if (config('app.debug')) {  // IF APP_DEBUG inside .env = true
+        //     DB::listen(fn ($query) => Log::info($query->sql, $query->bindings, $query->time));
+        // }
     }
 }
