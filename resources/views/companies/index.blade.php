310fe1a19a92828aaf31fe51b9ff35613654604b
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-15 10:46:38 +0100

    Refactoring views

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -23,18 +23,7 @@
             </div>
             <div class="card-body">
               {{-- @include('companies._filter') --}}
-              @if ($message = session('message'))
-                  <div class="alert alert-success">
-                    {{ $message }}
-                    @if ($undoRoute = session('undoRoute'))
-                      <form action="{{ $undoRoute }}" method="POST" style="display: inline">
-                        @csrf
-                        @method('delete')
-                        <button class="btn alert-link">Undo</button>
-                      </form>
-                    @endif
-                  </div>
-              @endif
+              @include('shared.flash')
               <table class="table table-striped table-hover">
                 <thead>
                   <tr>
@@ -58,8 +47,8 @@
                   @endphp
                   @forelse ($companies as $index => $company)
                     @include('companies._company', ['company' => $company, 'index' => $index])
-                    @empty
-                    @include('companies._empty')
+                  @empty
+                      @include('shared.empty', ['numCol' => 6])
                   @endforelse
                 </tbody>
               </table> 
