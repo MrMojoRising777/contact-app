422cd8b1326438da2752adefb2bb5b03164a71dd
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-15 12:39:03 +0100

    Adding feature to show company details

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -46,9 +46,9 @@ class CompanyController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Company $company)
     {
-        //
+        return view('companies.show', compact('company'));
     }
 
     /**
