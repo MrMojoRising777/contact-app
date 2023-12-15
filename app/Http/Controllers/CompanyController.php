8a5eb082c04fdad7e3a7320331dd6b2bedb7ce6d
MrMojoRising777 <alexander.goyens@gmail.com>
2023-12-15 15:39:09 +0100

    Implementing aggregate functions

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -18,6 +18,7 @@ class CompanyController extends Controller
             ->allowedSorts(['name', 'website', 'email'], '-id')
             ->allowedSearch('name', 'website', 'email')
             ->forUser(auth()->user())
+            ->withCount('contacts')
             ->paginate(10);
 
             return view('companies.index', compact('companies'));
